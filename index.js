6d700fc1eb8074538a80db7aa5db22392ca65dbe
prastham <anil.nal@gmail.com>
2016-07-08 16:12:42 +0800

    Create Echo Bot

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
+        if (!kittenMessage(event.sender.id, event.message.text)) {
+        sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
     }
+}
     res.sendStatus(200);
 });
 
